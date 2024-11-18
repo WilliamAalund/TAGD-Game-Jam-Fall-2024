48ef605ef20f341f8e390980b18ec7fd2096aee8
Aidan Thomas <aidanman0106@gmail.com>
2024-11-18 17:41:30 -0600

    some progress on the weapon ID system and Item system, still needs work

--- Game/Code/Scripts/Classes/Item.gd
+++ Game/Code/Scripts/Classes/Item.gd
@@ -3,16 +3,16 @@ extends Node
 class_name Item
 
 var nameItem
-var priceItem
+var weapIDItem
 var descriptionItem
 var modifierItem
 var lvlreqItem
 var typeItem
 
-func _init(item_name, item_price, item_description, item_modifier, item_lvlreq, item_type):
+func _init(item_name, item_weapID, item_description, item_modifier, item_lvlreq, item_type):
 	pass
 	self.nameItem = item_name
-	self.priceItem = item_price
+	self.weapIDItem = item_weapID
 	self.descriptionItem = item_description
 	self.modifierItem = item_modifier
 	self.lvlreqItem = item_lvlreq
---
+++ Game/Code/Scripts/TestScripts/WeaponDict.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var Weapons = {
+	1:
+	{
+		"Name": "Base",
+		"rpm": 10,
+		"auto": true,
+		"range": 500,
+		"velocity": 500,
+		"spread": 0,
+		"damage": 2,
+		"mode": 3
+		
+	},
+	2:
+	{
+		"Name": "Laser",
+		"rpm": 100,
+		"auto": true,
+		"range": 500,
+		"velocity": 1000,
+		"spread": 0,
+		"damage": 2,
+		"mode": 1
+	}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

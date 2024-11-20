bbb34c165fa607e5227f4843d6d1ffcaf09d174b
Aidan Thomas <aidanman0106@gmail.com>
2024-11-20 13:11:01 -0600

    health boost works now

--- Game/Code/Scripts/TestScripts/ShopTest.gd
+++ Game/Code/Scripts/TestScripts/ShopTest.gd
@@ -13,6 +13,8 @@ var selected_items = []
 
 @export var shop_level = 1
 @export var price = shop_level*15
+@export var multiplier = shop_level*8
+@export var health_multiplier = shop_level*1.1
 
 
 # Function to select random items ensuring each button displays a specific type
@@ -76,9 +78,8 @@ func _process(delta: float) -> void:
 
 
 func _on_item_button_pressed() -> void:
-	var item = item_data.Items[selected_items[0]]
-	if PlayerData.buyWeapon(item, price):
-		player_continue.emit()
+	PlayerData.buyWeapon(selected_items[0], price)
+	player_continue.emit()
 
 
 func _on_continue_button_pressed() -> void:
@@ -87,8 +88,10 @@ func _on_continue_button_pressed() -> void:
 
 
 func _on_item_button_2_pressed() -> void:
-	PlayerData.buyItem(selected_items[1], price)
+	PlayerData.buyItem(selected_items[1], price, multiplier)
+	player_continue.emit()
 
 
 func _on_item_button_3_pressed() -> void:
-	PlayerData.buyItem(selected_items[2], price)
+	PlayerData.buyItem(selected_items[2], price, health_multiplier)
+	player_continue.emit()
